95ba66037a92e7fce1388d7667617dcecbd075c1
Muntakim Ahmed Moon <148109529+nafisasraboni@users.noreply.github.com>
2025-06-02 23:38:24 +0600

    Update Vite
    Git updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/VibeSpace/', // 🔥 Add this line (case-sensitive)
+  base: '/VibeSpace/', 
   plugins: [react()],
   server: {
     proxy: {
